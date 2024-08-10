9951552936d3f3b9261e392ff806e31ca247f1eb
BANZAI1945 <shantadze99@gmail.com>
2024-08-10 09:18:12 -0700

    17

--- script/script.js
+++ script/script.js
@@ -1,4 +1,15 @@
-document.getElementById("bar").onclick = function() {
-    document.getElementById("bar").style.display = "none";
-    return false;
-    }
+let burger = document.querySelector(".burger");
+let xMark = document.querySelector(".xMark");
+let mobileMenu = document.querySelector(".header_nav");
+
+burger.addEventListener("click", () => {
+  burger.style.display = "none";
+  xMark.style.display = "block";
+  mobileMenu.classList.add("show");
+});
+
+xMark.addEventListener("click", () => {
+  xMark.style.display = "none";
+  burger.style.display = "block";
+  mobileMenu.classList.remove("show");
+});
